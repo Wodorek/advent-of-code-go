95c23c59a2bca82f0ad3290622be6393918bbbb9
Wodorek <krol.w94@gmail.com>
2024-11-27 01:04:05 +0100

    2015 day 4 part 4

--- 2015/04/main.go
+++ 2015/04/main.go
@@ -6,12 +6,26 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
+
+	"github.com/wodorek/advent-of-code-go/util"
 )
 
 //go:embed input.txt
 var input string
 
 func main() {
+
+	util.PrintSolution(1, p1())
+	util.PrintSolution(2, p2())
+
+}
+
+func getHash(str string) string {
+	hash := md5.Sum([]byte(str))
+	return hex.EncodeToString(hash[:])
+}
+
+func p1() string {
 	i := 0
 	for {
 		hash := getHash(input + strconv.Itoa(i))
@@ -21,11 +35,18 @@ func main() {
 		i++
 	}
 
-	fmt.Println(i)
-
+	return fmt.Sprintf("%d", i)
 }
 
-func getHash(str string) string {
-	hash := md5.Sum([]byte(str))
-	return hex.EncodeToString(hash[:])
+func p2() string {
+	i := 0
+	for {
+		hash := getHash(input + strconv.Itoa(i))
+		if hash[:6] == "000000" {
+			break
+		}
+		i++
+	}
+
+	return fmt.Sprintf("%d", i)
 }
