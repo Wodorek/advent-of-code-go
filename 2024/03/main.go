679ffb80ce6c84b35742f79357c56b4bad026363
Wodorek <krol.w94@gmail.com>
2024-12-03 18:58:40 +0100

    2024 day 3 part 2

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -13,6 +13,7 @@ var input string
 
 func main() {
 	util.PrintSolution(1, p1(input))
+	util.PrintSolution(2, p2(input))
 }
 
 func p1(inputStr string) string {
@@ -23,11 +24,7 @@ func p1(inputStr string) string {
 	for _, match := range matches {
 		re = regexp.MustCompile(`\d+`)
 		nums := re.FindAll(match, -1)
-		asInts := make([]int, 0)
-		for _, num := range nums {
-			x, _ := strconv.Atoi(string(num))
-			asInts = append(asInts, x)
-		}
+		asInts := covertToInts(nums)
 
 		for i := 0; i < len(asInts); i += 2 {
 			total += asInts[0] * asInts[i+1]
@@ -36,4 +33,46 @@ func p1(inputStr string) string {
 	}
 
 	return strconv.Itoa(total)
+}
+
+func p2(inputStr string) string {
+	instructions := regexp.MustCompile(`(mul\(\d+,\d+\))|(do\(\))|(don\'t\(\))`)
+
+	matches := instructions.FindAll([]byte(inputStr), -1)
+
+	operationsEnabled := true
+
+	total := 0
+	for _, match := range matches {
+		asStr := string(match)[:3]
+		switch asStr {
+		case "do(":
+			operationsEnabled = true
+		case "don":
+			operationsEnabled = false
+		case "mul":
+			if !operationsEnabled {
+				continue
+			}
+			re := regexp.MustCompile(`\d+`)
+			nums := re.FindAll(match, -1)
+			asInts := covertToInts(nums)
+
+			for i := 0; i < len(asInts); i += 2 {
+				total += asInts[0] * asInts[i+1]
+			}
+		}
+
+	}
+	return strconv.Itoa(total)
+}
+
+func covertToInts(bytes [][]byte) []int {
+	asInts := make([]int, 0)
+	for _, num := range bytes {
+		x, _ := strconv.Atoi(string(num))
+		asInts = append(asInts, x)
+	}
+
+	return asInts
 }
